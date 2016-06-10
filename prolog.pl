2d5049d6ae0dd48d6ae5cd746cb11f801341cc60
Gabriel Gimenez <12-11006@usb.ve>
2016-06-10 02:40:46 -0400

    Fixed, y casos de prueba

--- prolog.pl
+++ prolog.pl
@@ -1,6 +1,9 @@
 nodo(X,Y) :- integer(X), X>0, is_list(Y).
 arista(X,nodo(Y,Z)) :- integer(X), X>0, nodo(Y,Z).
 
+len([], 0).
+len([_|T], R) :- len(T, R1), R is R1+1. 
+
 bienEtiquetado(nodo(1,[])).
 bienEtiquetado(nodo(X,Y)) :- 
 	aplanar(nodo(X,Y), LN,LA,N),
@@ -24,9 +27,12 @@ nivel(nodo(X, [arista(Y, nodo(P,[])) | K]), LN, LA, N):-
 		append([Y], LA1, LA),
 		N is N1+1.
 nivel(nodo(X, [arista(Y, nodo(P,R)) | K]), LN, LA, N):- 
-		nivel(nodo(X, K), LN, LA1, N),
+		nivel(nodo(X, K), LN1, LA1, N1),
 		Y is abs(P-X),
-		append([Y], LA1, LA).
+		aplanar(nodo(P,R), LN2, LA2, N2),
+		append(LN1, LN2, LN),
+		N is N1+N2,
+		append([Y |LA2], LA1, LA).
 
 aplanar(nodo(X, []), [], [], 0).
 aplanar(nodo(X, [arista(Y, J) | K]), LN, LA, N):- 
@@ -34,5 +40,5 @@ aplanar(nodo(X, [arista(Y, J) | K]), LN, LA, N):-
 		aplanar(J, LN2, LA2, N2),
 		append(LN1, [X | LN2], LN),
 		append(LA1, LA2, LA),
-		N is N1+N2+1.
+		N is N1+1.
 
