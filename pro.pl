2c249111c43c1832dfca97da67f47d524e4b88f0
Gabriel Gimenez <12-11006@usb.ve>
2016-06-14 18:51:51 -0430

    Esqueleto

--- pro.pl
+++ pro.pl
@@ -1,5 +1,17 @@
-esq([Elem]).
+len([X], 1).
+len([_|Y], S) :- len(Y,R), S is R+1.	
 
+sumlist([], 0).
+sumlist([X | Y], R) :- sumlist(Y, P), R is X + P. 
 
-tail([elem], elem).
-tail([_|elemx], elem) :- tail(elemx, elem).
+esq([Y, X | S]) :- sumlist(Y, R), len(X,P), R =:= P, esq([X | S]).
+esq([X]) :- sumlist(X, R), R =:= 0.
+
+esqueleto(1, 0, esq([[1], [0]])).
+esqueleto(N, R,esq(X)) :- N1 is N-1, esqueleto(N1, 0, esq(Y)), append(Y,[[0]],X).
+
+ceros(0, []).
+ceros(Numero, Lista):- N1 is Numero-1, ceros(N1, L1), append([0], L1, Lista).
+
+%agregarNivel(N, N, R, LA, SUM, L):-
+%agregarNivel(Nmax, N, R, LA, SUM, L)
